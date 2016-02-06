b3d87454f47d96b64ea5f5120e24e07371f5c095
Nitin Madnani <nmadnani@ets.org>
2016-02-05 21:12:49 -0500

    Update task2 for nltk 3

--- task2.py
+++ task2.py
@@ -8,7 +8,7 @@ from random import choice
 cfd = ConditionalFreqDist()
 prev_word = None
 for word in gutenberg.words('austen-persuasion.txt'):
-    cfd[prev_word].inc(word)
+    cfd[prev_word][word] += 1
     prev_word = word
 
 word = 'therefore'
@@ -17,8 +17,8 @@ i = 1
 # Find all words that can possibly follow the current word
 # and choose one at random
 while i < 20:
-    print(word, end=" ")
-    lwords = list(cfd[word].samples())
+    print(word, end=' ')
+    lwords = list(cfd[word].keys())
     follower = choice(lwords)
     word = follower
     i += 1
